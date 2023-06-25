0a2aef5b53b9fd688483ebb4cbe4c3bbea149e16
Jyeverson <jyeversonsantos@gmail.com>
2023-06-25 02:35:52 -0300

    Novos exercicios resolvidos

---
+++ Solutions/Java/JavaDateAndTime.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class JavaDateAndTime {
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     * 1. INTEGER month
+     * 2. INTEGER day
+     * 3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(month, day, year);
+        return calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();
+
+    }
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = JavaDateAndTime.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ Solutions/Java/JavaStaticInitializerBlock.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JavaStaticInitializerBlock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int h = sc.nextInt();
+        if (b <= 0 || h <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        } else {
+            System.out.println(b * h);
+        }
+        sc.close();
+    }
+}
